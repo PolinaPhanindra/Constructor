7c38277cf555274e3f0cd70d7c99660d3a9b8c06
Bhagath Kummari <Bhagathchethan@gmail.com>
2018-03-27 22:12:25 -0400

    Add project files.

---
+++ Constructor/Constructor.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Constructor
+{
+    /// <summary>
+    /// Describes a class
+    /// </summary>
+
+    public class Constructor
+    {
+        #region Fields 
+        int lengthOfClass;//length
+        public int Count { get; set; }
+
+        public int Length
+        {
+            get
+            {
+                return lengthOfClass;
+            }
+
+            set
+            {
+                lengthOfClass = value;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// dadf
+        /// </summary>
+        /// <param name="lengthOfClass">asdf</param>
+        /// <param name="name">asdf</param>
+        public int CallingMethod(int lengthOfClass,
+                                     int a,
+                                     int b,
+                                     string asdf,
+
+                                     string name = "bhagatr")
+        {
+            int count = 0;
+            string fname = string.Empty;
+            try
+            {
+                if (asdf == null)
+                {
+                    return count;
+                }
+                asdf.Contains("as");
+
+                int y = 4 / count;
+                fname = "bhagar";
+                fname = fname.Substring(2);
+
+                StringBuilder mess = new StringBuilder(string.Empty);
+                //"This class length is of 789 characat. sos"
+                mess.ToString();
+
+                for (int i = fname.Length-1; i >= 0; i--)
+                {
+                    if (i == 2)
+                    {
+                        continue;
+                    }
+                }
+                IEnumerable<string> names = new List<string> { "c", "b","asdf"};
+                foreach (var item in names)
+                {
+                    
+                }
+               
+                
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return count;
+        }
+
+        public void Factorial(params int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        private int Test2(int a) {
+            return 9;
+        }
+    }
+        #endregion
+    }
+
+
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    /// <summary>
+    /// Describe a class
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person obj = new Constructor.Person("Bhagath","Kummari");
+            Adult obj = new Adult("bhagath","Kummari");
+            Constructor c1 = new Constructor();
+            
+            Console.WriteLine(Adult.minimumAge);
+            Console.ReadKey();
+            try
+            {
+
+            }
+            catch (NotImplementedException ex)
+            {
+                Console.WriteLine("");
+            }
+         
+        }
+    }
+    public class Person
+    {
+        private string last;
+        private string first;
+
+        public Person()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        public Person(string lastName, string firstName)
+        {
+            last = lastName;
+            first = firstName;
+            Console.WriteLine("the Full Name is {0}{1}", last, first);
+
+        }
+
+        // Remaining implementation of Person class.
+    }
+    public class Adult : Person
+    {
+        public static int minimumAge;
+
+        public Adult(string lastName, string firstName) : base(lastName, firstName)
+        {
+            Console.WriteLine("Adult overloaded constructor");
+            
+            
+        }
+
+        static Adult()
+        {
+            minimumAge = 18;
+        }
+        public Adult()
+        {
+
+        }
+
+        // Remaining implementation of Adult class.
+    }
+
+}
